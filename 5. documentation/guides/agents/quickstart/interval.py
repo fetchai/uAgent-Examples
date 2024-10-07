f769d7da11ca05f76780d1ce1316bad8710e4168
Joshua Croft <joshuacroft@Joshuas-MacBook-Pro.local>
2024-10-07 14:19:08 +0100

    initial page for docs code samples

---
+++ 5. documentation/guides/agents/quickstart/interval.py
@@ -0,0 +1,14 @@
+from uagents import Agent, Context
+
+# Create an agent named Alice
+alice = Agent(name="alice", seed="YOUR NEW PHRASE")
+
+# Define a periodic task for Alice
+@alice.on_interval(period=2.0)
+async def say_hello(ctx: Context):
+    ctx.logger.info(f'hello, my name is {alice.name}')
+
+
+# Run the agent
+if __name__ == "__main__":
+    alice.run()
