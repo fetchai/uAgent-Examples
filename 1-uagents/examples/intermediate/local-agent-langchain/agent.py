c829b0720412201210437d1886338f765064f89f
gautamgambhir97 <gautam.gambhir@fetch.ai>
2024-11-21 13:10:17 +0530

    feat: add examples for the intermediate topics

---
+++ 1-uagents/examples/intermediate/local-agent-langchain/agent.py
@@ -0,0 +1,42 @@
+from langchain_community.tools import WikipediaQueryRun
+from langchain_community.utilities import WikipediaAPIWrapper
+from uagents.setup import fund_agent_if_low
+from uagents import Agent, Context, Protocol, Model
+from uagents import Field
+from ai_engine import UAgentResponse, UAgentResponseType
+ 
+# Extend your protocol with Wikipedia data fetching
+class WikiReq(Model):
+    search_keyword: str = Field(description="This describes the keyword you want to search on wiki")
+ 
+SEED_PHRASE = "<Secret Phrase for your agent>"
+ 
+# Copy the address shown below
+print(f"Your agent's address is: {Agent(seed=SEED_PHRASE).address}")
+ 
+AGENT_MAILBOX_KEY = "Your_mailbox_address"
+ 
+# Now your agent is ready to join the agentverse!
+WikiAgent = Agent(
+    name="Wiki Agent",
+    seed=SEED_PHRASE,
+    mailbox=f"{AGENT_MAILBOX_KEY}@https://agentverse.ai",
+)
+fund_agent_if_low(WikiAgent.wallet.address()) #funding agent.
+wiki_protocol = Protocol("Wiki Protocol")
+ 
+@wiki_protocol.on_message(model=WikiReq, replies={UAgentResponse})
+async def load_dalle(ctx: Context, sender: str, msg: WikiReq):
+    wikipedia = WikipediaQueryRun(api_wrapper=WikipediaAPIWrapper())
+    ctx.logger.info(msg.search_keyword)
+    try:
+        result = wikipedia.run(msg.search_keyword)
+    except Exception as e:
+        ctx.logger.info(f"Error generating response: {e}")
+    # Send an error response back to the user
+    await ctx.send(
+        sender, UAgentResponse(message=str(result), type=UAgentResponseType.FINAL)
+    )
+ 
+WikiAgent.include(wiki_protocol, publish_manifest=True)
+WikiAgent.run()
