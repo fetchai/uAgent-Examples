6c2724e70290690d9a0bdfe520ee8812e68dd001
Ali Hosseini <ali@MBP24.local>
2025-03-03 15:43:50 +0000

    chore: sync examples

--- 1-uagents/examples_existing/09-booking-protocol-demo/query.py
+++ 1-uagents/examples_existing/09-booking-protocol-demo/query.py
@@ -2,19 +2,15 @@ import asyncio
 
 from protocols.query import GetTotalQueries, TotalQueries
 
-from uagents.envelope import Envelope
 from uagents.query import query
 
 RESTAURANT_ADDRESS = "agent1qfpqn9jhvp9cg33f27q6jvmuv52dgyg9rfuu37rmxrletlqe7lewwjed5gy"
 
 
 async def main():
-    response = await query(RESTAURANT_ADDRESS, GetTotalQueries())
-    if isinstance(response, Envelope):
-        msg = TotalQueries.parse_raw(response.decode_payload())
-        print(f"Query response: {msg.json()}\n\n")
-    else:
-        print(f"Error: {response}")
+    env = await query(RESTAURANT_ADDRESS, GetTotalQueries())
+    msg = TotalQueries.parse_raw(env.decode_payload())
+    print(f"Query response: {msg.json()}\n\n")
 
 
 if __name__ == "__main__":
